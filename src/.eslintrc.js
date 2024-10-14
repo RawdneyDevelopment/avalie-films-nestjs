749fe5f021c58ecc4158d36fe8000396a124cd8e
Rawdney Mendes <mendesrawdney@gmail.com>
2024-10-14 13:49:13 -0300

    configuration .eslint  and prettier

---
+++ src/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      project: 'tsconfig.json',
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    root: true,
+    env: {
+      node: true,
+      jest: true,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };
+  
